bf84eec6095dc2e093b9ae9a1f2706cd3f969921
Heithor <contato.hecthorur@gmail.com>
2024-11-21 13:58:32 -0300

    chore: adding tailwind to the project

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,16 @@ import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-d
 import TravelRequest from './components/TravelRequest';
 import TravelOptions from './components/TravelOptions';
 import TravelHistory from './components/TravelHistory';
+import './App.css'; // Import the CSS file
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/solicitacao" element={<TravelRequest />} />
-        <Route path="/opcoes" element={<TravelOptions />} />
-        <Route path="/historico" element={<TravelHistory />} />
-        <Route path="*" element={<Navigate to="/solicitacao" />} />
+        <Route path='/solicitacao' element={<TravelRequest />} />
+        <Route path='/opcoes' element={<TravelOptions />} />
+        <Route path='/historico' element={<TravelHistory />} />
+        <Route path='*' element={<Navigate to='/solicitacao' />} />
       </Routes>
     </Router>
   );
